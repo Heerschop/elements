2eac4457386b8315c4be5e2f8e8257d4c304e1d3
Bas Heerschop <heerschop@gmail.com>
2023-01-31 06:33:16 +0100

    .

--- storybook/src/story-Button.stories.tsx
+++ storybook/src/story-Button.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import { useArgs } from '@storybook/client-api';
 import { Button } from './story-Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -14,7 +15,21 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = args => {
+  const [{ primary }, updateArgs] = useArgs();
+
+  return (
+    <Button
+      {...args}
+      onClick={() => {
+        updateArgs({
+          primary: !primary,
+          backgroundColor: primary ? '#ff0000' : '#00ff00',
+        });
+      }}
+    />
+  );
+};
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
