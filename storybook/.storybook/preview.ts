1812fe08a66a3f8f4c49475942d09d2606cce3da
Bas Heerschop <heerschop@gmail.com>
2023-04-22 08:34:03 +0200

    .

--- storybook/.storybook/preview.ts
+++ storybook/.storybook/preview.ts
@@ -1,4 +1,21 @@
 import type { Preview } from '@storybook/react';
+import '../src/global.css';
+import { themes } from '@storybook/theming';
+import { StoryContext } from '@storybook/types';
+
+function sanitise(source: string): string {
+  const patterns = [
+    // [/\s\w+\=false/g, ''], // Removes all attributes with a value of "false"
+    // [/(\s)(\w+)\=true/g, '$1$2'], // Removes the value of attributes which are "true"
+    [/\{(\d+)\}/g, '"$1"'], // Replace {8} for "8"
+  ];
+
+  for (const [pattern, value] of patterns) {
+    source = source.replace(pattern, value as string);
+  }
+
+  return source;
+}
 
 const preview: Preview = {
   parameters: {
@@ -7,6 +24,14 @@ const preview: Preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
+      },
+    },
+    docs: {
+      theme: themes.dark,
+      source: {
+        transformSource: (source: string, storyContext: StoryContext) =>
+          storyContext.viewMode === 'docs' ? storyContext.undecoratedStoryFn(storyContext) : sanitise(source),
+        excludeDecorators: true,
       },
     },
   },
