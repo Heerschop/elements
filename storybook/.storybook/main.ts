e5026c78f8f09df01bd2b43cc25d097137a2d437
Bas Heerschop <heerschop@gmail.com>
2023-04-23 09:53:12 +0200

    .

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -2,14 +2,17 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  typescript: {
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      shouldRemoveUndefinedFromOptional: true,
+      exclude: ['node_modules/**'],
+    },
   },
   docs: {
     autodocs: 'tag',
